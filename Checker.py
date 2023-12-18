ccfa17cf4b9660cc4c9123d6f66857d88423ea28
Mohit R <153975303+mohit1402@users.noreply.github.com>
2023-12-18 11:56:54 +0530

    Odd even Checker.py

--- Checker.py
+++ Checker.py
@@ -1,13 +1,5 @@
-# Take input from the user
-
 num = int(input("Enter a number: "))
-
-# Check if the number is even or odd
-
 if num % 2 == 0:
-
   print(num, "is even.")
-
 else:
-
   print(num, "is odd.")
